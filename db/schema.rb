b2c41ef815bcb9e511538187aeb8ee419e58cbc8
Varun Shivakumar <varunshivakumar@outlook.com>
2016-04-23 13:47:09 -0400

    Schema changed and added ./idea/workspace.xml to gitignore

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416205145) do
+ActiveRecord::Schema.define(version: 20160423050908) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.integer  "client_id_id",   limit: 4
+    t.integer  "employee_id_id", limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "accounts", ["client_id_id"], name: "index_accounts_on_client_id_id", using: :btree
+  add_index "accounts", ["employee_id_id"], name: "index_accounts_on_employee_id_id", using: :btree
 
   create_table "clients", force: :cascade do |t|
     t.integer  "rating",         limit: 4
     t.integer  "credit_card_no", limit: 4
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.text     "bio",         limit: 65535
+    t.integer  "hourly_rate", limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  create_table "hidden_stop_orders", force: :cascade do |t|
+    t.integer  "price_per_share", limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
   create_table "locations", force: :cascade do |t|
@@ -31,6 +54,51 @@ ActiveRecord::Schema.define(version: 20160416205145) do
   end
 
   add_index "locations", ["user_id"], name: "index_locations_on_user_id", using: :btree
+
+  create_table "market_on_close_orders", force: :cascade do |t|
+    t.string   "buy_sell_type", limit: 255
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  create_table "market_orders", force: :cascade do |t|
+    t.string   "buy_sell_type", limit: 255
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "num_shares",      limit: 4
+    t.integer  "price_per_share", limit: 4
+    t.integer  "transaction_fee", limit: 4
+    t.boolean  "completed"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  create_table "owns_stocks", force: :cascade do |t|
+    t.integer  "num_shares",    limit: 4
+    t.integer  "bought_price",  limit: 4
+    t.integer  "account_id_id", limit: 4
+    t.integer  "stock_id_id",   limit: 4
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  create_table "stocks", force: :cascade do |t|
+    t.string   "company_name",    limit: 255
+    t.string   "stock_symbol",    limit: 255
+    t.string   "stock_type",      limit: 255
+    t.integer  "price_per_share", limit: 4
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
+
+  create_table "trailing_stop_orders", force: :cascade do |t|
+    t.integer  "price_per_share", limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name",      limit: 255
