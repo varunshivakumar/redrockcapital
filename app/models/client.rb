468b8bd0dbda9b7be56093f6c6a75ec50964ac10
Varun Shivakumar <varunshivakumar@outlook.com>
2016-04-23 12:02:21 -0400

    Created a relationship between client and employee through account

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,4 @@
 class Client < User
   has_one :account
+  has_one :employee, through: :account
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,4 @@
 class Employee < User
   has_many :accounts
+  has_many :clients, through: :accounts
 end
